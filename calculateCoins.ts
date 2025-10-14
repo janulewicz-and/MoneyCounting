46328e6896f6d7f68b582eb1625f0accdd3d2218
Andrzej Janulewicz <and.janulewicz@gmail.com>
2025-10-14 15:24:48 +0200

    now user can add his own denomination

--- calculateCoins.ts
+++ calculateCoins.ts
@@ -1,4 +1,4 @@
-export type Denomination = 1 | 2 | 5 | 10 | 20 | 50;
+export type Denomination = number;
 
 export interface coinsInfo {
   rolls: number;
@@ -17,30 +17,36 @@ const coinPerRoll: Record<Denomination, number> = {
 } as const;
 
 export function calculateCoins(arr: Denomination[]): bank {
-  const amountOfCoins: Record<Denomination, number> = {
-    1: 0,
-    2: 0,
-    5: 0,
-    10: 0,
-    20: 0,
-    50: 0,
-  };
+  const zeroValue: Record<Denomination, number> = Object.keys(
+    coinPerRoll
+  ).reduce((acc, key) => {
+    acc[Number(key)] = 0;
+    return acc;
+  }, {} as Record<Denomination, number>);
+
   arr.forEach((coin) => {
-    amountOfCoins[coin] += 1;
+    zeroValue[coin] += 1;
   });
-  const result: bank = {
-    1: { rolls: 0, rest: 0 },
-    2: { rolls: 0, rest: 0 },
-    5: { rolls: 0, rest: 0 },
-    10: { rolls: 0, rest: 0 },
-    20: { rolls: 0, rest: 0 },
-    50: { rolls: 0, rest: 0 },
-  };
-  Object.entries(amountOfCoins).forEach(([key, amount]) => {
+  const result = Object.fromEntries(
+    Object.keys(coinPerRoll).map((key) => [Number(key), { rolls: 0, rest: 0 }])
+  ) as bank;
+
+  Object.entries(zeroValue).forEach(([key, amount]) => {
     const denom = Number(key) as Denomination;
     const perRoll = coinPerRoll[denom];
     result[denom].rolls = Math.floor(amount / perRoll);
     result[denom].rest = amount % perRoll;
   });
+  // const result: bank = Object.keys(coinPerRoll).reduce((acc, key) => {
+  //   const denom = Number(key) as Denomination;
+  //   const amount = zeroValue[denom] ?? 0;
+  //   const perRoll = coinPerRoll[denom];
+  //   acc[denom] = {
+  //     rolls: Math.floor(amount / perRoll),
+  //     rest: amount % perRoll,
+  //   };
+  //   return acc;
+  // }, {} as bank);
+
   return result;
 }
